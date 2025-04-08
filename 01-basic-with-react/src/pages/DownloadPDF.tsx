efbc9740b8b996719a160f44a7ddc18bf397cbe1
uniqweber <uniqwebersocial@gmail.com>
2025-04-09 00:26:34 +0600

    client component created

--- 01-basic-with-react/src/pages/DownloadPDF.tsx
+++ 01-basic-with-react/src/pages/DownloadPDF.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {Page, Text, View, Document,  PDFDownloadLink, Image} from "@react-pdf/renderer";
 import {Button} from "../components/ui/Button";
 import prescriptionData from "../utils/data/prescription.json";
